5e81a69ee527afdb5cb7cbba98b19f79b6dfe03d
Martin Nestorov <mnestorov@users.noreply.github.com>
2024-10-24 16:38:15 +0300

    Update smarty-form-submissions.php

--- smarty-form-submissions.php
+++ smarty-form-submissions.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link                    https://github.com/mnestorov
+ * @link                    https://github.com/mnestorov/smarty-form-submissions
  * @since                   1.0.0
  * @package                 Smarty_Form_Submissions
  *
